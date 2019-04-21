d17bb49ead70ac191c5c95b6057d9b2165360fed
Zwerew Anton <anton.zwerew@gmail.com>
2019-04-21 21:56:34 +0300

    Контакт выделен в отдленый класс

---
+++ contact.py
@@ -0,0 +1,30 @@
+class Contact():
+    def __init__(self, first_name, middle_name, last_name, nickname, photo, title, company, address1, phone_home,
+ phone_mobile, phone_work, phone_fax, email1, email2, email3, homepage, bday_day, bday_month, bday_year,
+ aday_day, aday_month, aday_year, group, address2, phone2, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.photo = photo
+        self.title = title
+        self.company = company
+        self.address1 = address1
+        self.phone_home = phone_home
+        self.phone_mobile = phone_mobile
+        self.phone_work = phone_work
+        self.phone_fax = phone_fax
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.bday_day = bday_day
+        self.bday_month = bday_month
+        self.bday_year = bday_year
+        self.aday_day = aday_day
+        self.aday_month = aday_month
+        self.aday_year = aday_year
+        self.group = group
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
