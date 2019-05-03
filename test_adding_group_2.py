e4ed97e7dc7df82bed4848221bffb258737b6175
Anton Zverev <anton.zwerew@gmail.com>
2019-05-03 18:01:03 +0300

    Изменен селектор на более читаемый

--- test_adding_group_2.py
+++ test_adding_group_2.py
@@ -18,8 +18,7 @@ class TestAddingGroup2(unittest.TestCase):
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
+        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_groups_page(self, wd):
         wd.find_element_by_name("new").click()
