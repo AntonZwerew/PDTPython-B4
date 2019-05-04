b6c90dd4239c7b781837b0d6574e10d01456abcd
Anton Zverev <anton.zwerew@gmail.com>
2019-05-04 15:34:58 +0300

    Классы - фикслуры объединены в один, в котором создаются все помощникн

--- fixture/application.py
+++ fixture/application.py
@@ -1,16 +1,15 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
+from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
-
 class Application:
-
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.contact = ContactHelper(self)
-
+        self.group = GroupHelper(self)
 
     def destroy(self):
         self.wd.quit()
