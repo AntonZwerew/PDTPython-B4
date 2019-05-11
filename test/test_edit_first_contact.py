1fabc8112b4bce14159fc4ba54bddc6f64efa236
Anton Zverev <anton.zwerew@gmail.com>
2019-05-11 12:40:20 +0300

    Добавлены тесты редактирования первого контакта и первой группы. Тесты доработаны, запускаются корректно

---
+++ test/test_edit_first_contact.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
+def test_adding_contact(app):
+    edited_contact = Contact("Petr", "Petrovich", "Petrov", "Petya", "/opt/lampp/htdocs/addressbook/title.png", "Title2",
+                    "Macrosoft", "Moscow, Leningradsky rail terminal", "8-800-999-99-99", "89859999999", "+7(800)999-99-99",
+                    "++7788000099999999999999", "petya@fsb.ru", "Petr@kgb.su", "petrovich@petr.petr", "yandex.io", "16",
+                    "December", "1999", "15", "December", "2000", "Name2", "Adrees", "hfh",
+                    "jdjfjfjkafj!")
+    app.session.login("admin", "secret")
+    app.contact.edit_first(edited_contact)
+    app.session.logout()
