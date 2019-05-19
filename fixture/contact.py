183bea857fbce0875baea4a1b145515e12229961
Anton Zverev <anton.zwerew@gmail.com>
2019-05-19 12:42:29 +0300

    При открытии главной страницы (с контатами) добавлена дополнительная проверка

--- fixture/contact.py
+++ fixture/contact.py
@@ -9,7 +9,7 @@ class ContactHelper:
 
     def open_main_page(self):
         wd = self.app.wd
-        if not wd.current_url.endswith("/index.php"):
+        if not (wd.current_url.endswith("/index.php") and len(wd.find_elements_by_link_text("All e-mail")) > 0):
             wd.get("http://localhost/addressbook/")
 
     def add(self, contact):
